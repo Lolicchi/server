b2e8532cdd5b8e625d9aba9911e675cbbb6cbfbe
Jamie Fer <67526745+Lolicchi@users.noreply.github.com>
2022-08-02 20:37:58 +0530

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -2,13 +2,13 @@ import { serve } from 'https://deno.land/std/http/server.ts'
 
 try {
   let port = Number(Deno.env.get('PORT')) || 8080,
-    res = '{ x: Deno Server' + Deno.pid + ', at: ' + port + ' }'
+    res = ['{ x: Deno Server ', Deno.pid, ', at: ', port, ' }']
 
-  serve(() => new Response(res), {
+  serve(() => new Response(res.join('')), {
     port
   })
   
-  console.log(res)
+  console.log(...res)
 } catch (err) {
   console.log(err)
 }
